c7534074428d4d07583d23df8aab4959342e74d1
hr943192@gmail.com <hr943192@gmail.com>
2026-07-24 18:48:34 +0530

    Hotel Booking System - React + Node + MySQL

---
+++ hotel-frontend/src/App.js
@@ -0,0 +1,12 @@
+import RoomsList from './RoomsList';
+
+function App() {
+  return (
+    <div>
+      <h1 style={{textAlign: 'center'}}>Hotel Booking System </h1>
+      <RoomsList />
+    </div>
+  );
+}
+
+export default App;
