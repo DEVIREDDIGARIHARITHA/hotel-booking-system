60fe8847810e3feca466cc0282f4f9c1ab37f07f
DEVIREDDIGARIHARITHA <hr943192@gmail.com>
2026-07-25 20:19:47 +0530

    Fix import path and remove extra space in header

--- hotel-frontend/src/App.js
+++ hotel-frontend/src/App.js
@@ -1,12 +1,11 @@
-import RoomsList from './RoomsList';
+import RoomsList from './RoomsList.jsx';
 
 function App() {
   return (
     <div>
-      <h1 style={{textAlign: 'center'}}>Hotel Booking System </h1>
+      <h1 style={{textAlign: 'center'}}>Hotel Booking System</h1>
       <RoomsList />
     </div>
   );
 }
-
 export default App;
