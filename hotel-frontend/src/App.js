e2d3ff140814bdd2b95bcc6a44333f76c6699199
DEVIREDDIGARIHARITHA <hr943192@gmail.com>
2026-07-26 07:30:38 +0530

    Remove inline style from header in App component

--- hotel-frontend/src/App.js
+++ hotel-frontend/src/App.js
@@ -3,7 +3,7 @@ import RoomsList from './RoomsList.jsx';
 function App() {
   return (
     <div>
-      <h1 style={{textAlign: 'center'}}>Hotel Booking System</h1>
+      <h1>Hotel Booking System</h1>
       <RoomsList />
     </div>
   );
