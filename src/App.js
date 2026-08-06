07fc21a9f994beb7193f99b47a9f519cd5325948
hr943192@gmail.com <hr943192@gmail.com>
2026-08-06 21:12:49 +0530

    Fix gitignore and remove cache

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div style={{textAlign: 'center', padding: '50px', fontFamily: 'Arial'}}>
+      <h1>Hotel Booking System</h1>
+      <p>Deploy Success! 🎉</p>
+    </div>
+  );
+}
+export default App;
